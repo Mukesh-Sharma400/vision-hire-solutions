8920ca7ec056d6b0e2a93df97302e7d2c4dff249
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2025-01-10 21:10:01 +0530

    SearchBox

---
+++ src/app/components/SearchBox.js
@@ -0,0 +1,81 @@
+"use client";
+
+import styled from "styled-components";
+
+export default function SearchBox() {
+  return (
+    <DisplayWrapper>
+      <SearchIcon>
+        <i className="bi bi-search"></i>
+      </SearchIcon>
+      <SearchInput placeholder="Job title or keyword" />
+      <SearchBtn>Search</SearchBtn>
+    </DisplayWrapper>
+  );
+}
+
+const DisplayWrapper = styled.div`
+  width: 70%;
+  min-width: 450px;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin: -35px auto;
+  padding: 10px;
+  border-radius: 40px;
+  background: white;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  }
+`;
+
+const SearchIcon = styled.div`
+  width: 50px;
+  height: 50px;
+  min-width: 50px;
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+  color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+
+  @media (max-width: 450px) {
+    display: none;
+  }
+`;
+
+const SearchInput = styled.input`
+  width: 100%;
+  color: black;
+  font-size: 20px;
+
+  &::placeholder {
+    color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const SearchBtn = styled.button`
+  width: fit-content;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-color: black;
+  font-size: 20px;
+  padding: 0 20px;
+  border-radius: 30px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #6598f3;
+  }
+`;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "./sections/Hero";
+import SearchBox from "./components/SearchBox";
 import BaseLayout from "./components/BaseLayout";
 
 export default function Home() {
   return (
     <BaseLayout>
       <Hero />
+      <SearchBox />
     </BaseLayout>
   );
 }
